c494647ed426213dd59f1c1991ef52d6cbe38b1b
Raxabi <beltrandiaztorres@gmail.com>
2022-09-18 20:09:48 +0000

    [FIX] Restart the countdown and the 'message_count list' when the timer reach 0

--- src/bot.py
+++ src/bot.py
@@ -37,7 +37,9 @@ async def on_message(message: Message):
             timer = timer - 1
             await asyncio.sleep(1)
             print(timer, end="\r")
-    
+        if timer == 0:
+            timer = 24 * 3600
+            message_count.clear()
     if len(message_count) > 5:
         await message.channel.send(f"Aun quedan {datetime.timedelta(hours = timer / 3600)} horas para que el sol vuelva a salir por ESPAÑA")
         await message.delete(delay=1/100000)
